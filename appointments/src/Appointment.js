97daa79c948c915366a3e6046c4cdba25dc4882f
unknown <jspeci@github.com>
2021-07-13 16:26:40 -0700

    appointments-list-added

--- appointments/src/Appointment.js
+++ appointments/src/Appointment.js
@@ -1,5 +1,28 @@
 import React from 'react';
 
-export const Appointment = ({ customer }) => (
+
+const appointmentTimeOfDay = startsAt => {
+  const [h, m] = new Date(startsAt).toTimeString().split(':');
+  return `${h}:${m}`;
+}
+
+export const Appointment = ({ startsAt,customer }) => (
   <div>{customer.firstName}</div>
 );
+
+export const AppointmentsDayView = ({ appointments }) => <div id="appointments-day-view">
+  {appointments.length === 0 ? (
+    <p>There are no appointments scheduled for today.</p>
+  ) : (
+    <Appointment {...appointments[0]} />
+  )}
+  <ol>
+    {
+      appointments.map(i => <li key={i.startsAt}>
+        {
+          appointmentTimeOfDay(i.startsAt)
+        }
+      </li>)
+    }
+  </ol>
+</div>;
