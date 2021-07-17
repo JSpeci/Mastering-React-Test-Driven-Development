812fa79f3c3c656d669bbd4a391188baa93cdf10
unknown <jspeci@github.com>
2021-07-17 16:36:10 +0200

    day view finished

--- appointments/src/Appointment.js
+++ appointments/src/Appointment.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 const appointmentTimeOfDay = startsAt => {
@@ -6,23 +6,35 @@ const appointmentTimeOfDay = startsAt => {
   return `${h}:${m}`;
 }
 
-export const Appointment = ({ startsAt,customer }) => (
+export const Appointment = ({ startsAt, customer }) => (
   <div>{customer.firstName}</div>
 );
 
-export const AppointmentsDayView = ({ appointments }) => <div id="appointments-day-view">
-  {appointments.length === 0 ? (
-    <p>There are no appointments scheduled for today.</p>
-  ) : (
-    <Appointment {...appointments[0]} />
-  )}
-  <ol>
-    {
-      appointments.map(i => <li key={i.startsAt}>
+export const AppointmentsDayView = ({ appointments }) => {
+  const [selectedAppointment, setSelectedAppointment] = useState(
+    0
+  );
+
+  return (
+    <div id="appointments-day-view">
+      {appointments.length === 0 ? (
+        <p>There are no appointments scheduled for today.</p>
+      ) : (
+        <Appointment {...appointments[selectedAppointment]} />
+      )}
+      <ol>
         {
-          appointmentTimeOfDay(i.startsAt)
+          appointments.map((app, i) => <li key={app.startsAt}>
+            <button type="button"
+              onClick={() => setSelectedAppointment(i)}
+            >
+              {
+                appointmentTimeOfDay(app.startsAt)
+              }
+            </button>
+          </li>)
         }
-      </li>)
-    }
-  </ol>
-</div>;
+      </ol>
+    </div>
+  );
+};
